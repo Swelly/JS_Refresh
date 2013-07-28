51d56d20f250dcdc8cc9c9a0e36c6713328481a2
Wesley Carr <surfswell17@gmail.com>
2013-07-28 13:43:53 -0400

    "going through global variables and if else functions"

--- intro.js
+++ intro.js
@@ -1,2 +1,35 @@
-alert('hey it worked!');
-var h1 = document.getElementsByTagName('h1')[0];
+// 'window' object is the "GOD" object
+// global variables get attached to the window
+window.onload = function () {
+  var h1 = document.getElementsByTagName('h1')[0];
+  alert('hey it worked!');
+};
+
+// Primitive datatypes
+var myName = 'Wes';
+var myAge = 20;
+var isLazy = false;
+  // 'global' all caps
+var MY_TRUE_LOVE = 'Pashmeena';
+// null
+var lifting;
+// undefined - errors
+liftingIsFun = true;
+
+if (liftingIsFun) {
+  console.log("i love to lift");
+} else {
+  console.log("i hate to lift");
+}
+
+// if else | else if
+if (myAge < 150) {
+  //some code
+  "I'm alive";
+} else if (myAge > 0) {
+  // mo code
+  "I'm alive";
+} else {
+  //final code
+  "probably dead :(";
+}
